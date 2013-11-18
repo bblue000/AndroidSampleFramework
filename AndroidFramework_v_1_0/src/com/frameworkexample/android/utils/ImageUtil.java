3457601e7b9d47d319e392065456e4a9382a3e17
YinYong <yy15151877621@126.com>
2013-11-18 16:25:00 +0800

    [Update] 增加项目中的LogUtils

--- AndroidFramework_v_1_0/src/com/frameworkexample/android/utils/ImageUtil.java
+++ AndroidFramework_v_1_0/src/com/frameworkexample/android/utils/ImageUtil.java
@@ -1,20 +1,24 @@
 package com.frameworkexample.android.utils;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.lang.ref.PhantomReference;
 import java.lang.ref.ReferenceQueue;
 
-import com.frameworkexample.android.network.HttpClientUtil;
-import com.frameworkexample.android.network.SendRequestUtil;
-
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.Bitmap.Config;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.support.v4.util.LruCache;
-import android.util.Log;
 import android.widget.ImageView;
+
+import com.frameworkexample.android.network.HttpClientUtil;
+import com.frameworkexample.android.network.SendRequestUtil;
 
 public class ImageUtil {
 	private static final String TAG = ImageUtil.class.getSimpleName();
@@ -30,7 +34,7 @@ public class ImageUtil {
 	}
 	private ImageUtil() {
 		if (imageCache == null) {
-			Log.i("imageCache", "init imageCache");
+			LogUtils.i("imageCache", "init imageCache");
 			int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
 			int cacheSize = maxMemory / 8;
 			imageCache = new LruCache<String, Bitmap>(cacheSize) {
@@ -51,7 +55,7 @@ public class ImageUtil {
 							new ReferenceQueue<Bitmap>());
 					oldValue = null;
 					System.gc();
-					Log.i("imageCache", "entryRemoved old" + oldValue);
+					LogUtils.i("imageCache", "entryRemoved old" + oldValue);
 				}
 			};
 		}
@@ -62,11 +66,11 @@ public class ImageUtil {
 			synchronized (imageCache) {
 				imageCache.put(url, bitmap);
 			}
-			Log.i("imageCache", "imageCache size--->" + imageCache.size());
-			Log.i("imageCache", "imageCache maxsize--->" + imageCache.maxSize());
-			Log.i("imageCache",
+			LogUtils.i("imageCache", "imageCache size--->" + imageCache.size());
+			LogUtils.i("imageCache", "imageCache maxsize--->" + imageCache.maxSize());
+			LogUtils.i("imageCache",
 					"imageCache putCount--->" + imageCache.putCount());
-			Log.i("imageCache",
+			LogUtils.i("imageCache",
 					"imageCache evictionCount--->" + imageCache.evictionCount());
 		}
 	}
@@ -115,7 +119,7 @@ public class ImageUtil {
 				if (imageCache.get(url) != null) {
 					final Bitmap bitmap = imageCache.get(url);
 					if (bitmap != null) {
-						Log.i(TAG, "setImage  get bitmap by memory !!!"
+						LogUtils.i(TAG, "setImage  get bitmap by memory !!!"
 								+ Thread.currentThread().getId());
 						if(type==IMAGE_SRC)
 						{
@@ -145,7 +149,7 @@ public class ImageUtil {
 					LocalFileUtility.FILE_IMG_PATH);
 			if (f != null) {
 				if (f.length() <= 0) {
-					Log.i("FileException:", "f.delete");
+					LogUtils.i("FileException:", "f.delete");
 					f.delete();
 				}
 				// 第二优先级 尝试取本地
@@ -153,7 +157,7 @@ public class ImageUtil {
 				if (bmp != null) {
 					addCacheUrl(url, bmp);
 					if (bmp != null) {
-						Log.i(TAG,"setImage  get bitmap by sdcard or storage !!!");
+						LogUtils.i(TAG,"setImage  get bitmap by sdcard or storage !!!");
 						if(type==IMAGE_SRC)
 						{
 							iv.setImageBitmap(bmp);
@@ -184,16 +188,16 @@ public class ImageUtil {
 		try {
 			String filepath = LocalFileUtility.getFilePath(context, paramPath);
 			cacheFile = new File(filepath + fileName);
-			Log.i(TAG, " createImageCacheFile filepath: " + filepath
+			LogUtils.i(TAG, " createImageCacheFile filepath: " + filepath
 					+ " cacheFile: " + cacheFile);
 			if (cacheFile.exists()) {
-				Log.i(TAG, "cacheFile exists!!!");
+				LogUtils.i(TAG, "cacheFile exists!!!");
 				cacheFile = HttpClientUtil.downloadImageFile(context, url,
 						cacheFile);
 				return cacheFile;
 			} else {
 				if (!new File(filepath).exists()) {
-					Log.w("TAG", "mkdirs");
+					LogUtils.w("TAG", "mkdirs");
 					new File(filepath).mkdirs();
 				}
 				cacheFile = HttpClientUtil.downloadImageFile(context, url,
@@ -201,11 +205,149 @@ public class ImageUtil {
 			}
 		} catch (Exception e) {
 			cacheFile = null;
-			Log.e(TAG,
+			LogUtils.e(TAG,
 					" FileUtil createImageCacheFile Exception "
 							+ e.getMessage());
 			e.printStackTrace();
 		}
 		return cacheFile;
+	}
+	
+	public static Bitmap getBitmapFromRes(Context context, int resId) {
+		Bitmap bitmap = null;
+		try {
+			BitmapFactory.Options options = new BitmapFactory.Options();
+			options.inPreferredConfig = Config.RGB_565;
+			options.inPurgeable = true;
+			options.inInputShareable = true;
+			bitmap = BitmapFactory.decodeResource(context.getResources(),
+					resId, options);
+		} catch (Exception e) {
+			bitmap = null;
+			LogUtils
+					.e(TAG, "getBitmapFromRes Exception: " + e.getMessage());
+		}
+		return bitmap;
+	}
+
+	public static Bitmap getBitmapFromFileInputStream(FileInputStream is) {
+		if (is == null)
+			return null;
+		Bitmap bitmap = null;
+		try {
+			BitmapFactory.Options options = new BitmapFactory.Options();
+			options.inPreferredConfig = Config.RGB_565;
+			options.inPurgeable = true;
+			options.inInputShareable = true;
+			bitmap = BitmapFactory.decodeFileDescriptor(is.getFD(), null,
+					options);
+		} catch (Exception e) {
+			bitmap = null;
+			LogUtils
+					.e(TAG,
+							"getBitmapFromFileInputStream Exception: "
+									+ e.getMessage());
+		} finally {
+			if (is != null) {
+				try {
+					is.close();
+				} catch (Exception e) {
+				}
+			}
+		}
+		return bitmap;
+	}
+
+	public static Bitmap getBitmapFromFile(String filePath) {
+		return getBitmapFromFile(new File(filePath));
+	}
+
+	public static Bitmap getBitmapFromFile(File file) {
+		FileInputStream fis;
+		try {
+			fis = new FileInputStream(file);
+		} catch (Exception e) {
+			LogUtils.e(TAG,
+					"getBitmapFromFile Exception: " + e.getMessage());
+			return null;
+		}
+		return getBitmapFromFileInputStream(fis);
+	}
+
+	/**
+	 * 将Bitmap转化为字节数组
+	 * 
+	 * @param bmp
+	 *            target bitmap to read
+	 * @param needRecycle
+	 *            是否需要回收bitmap
+	 * @added 1.0
+	 */
+	public static byte[] bmpToByteArray(final Bitmap bmp,
+			final boolean needRecycle) {
+		if (null == bmp) {
+			return null;
+		}
+		ByteArrayOutputStream output = null;
+		try {
+			output = new ByteArrayOutputStream();
+			bmp.compress(CompressFormat.PNG, 100, output);
+			if (needRecycle) {
+				bmp.recycle();
+			}
+			byte[] result = output.toByteArray();
+			return result;
+		} catch (Exception e) {
+			LogUtils.e(TAG, "bmpToByteArray Exception: " + e.getMessage());
+			return null;
+		} finally {
+			if (null != output) {
+				try {
+					output.close();
+				} catch (Exception ex) {
+				}
+			}
+		}
+	}
+
+	public static byte[] scaleBitmapIfNeededToSize(Bitmap bitmap, long size) {
+		byte[] data = null;
+		try {
+			float width = bitmap.getWidth();
+			float height = bitmap.getHeight();
+			data = bmpToByteArray(bitmap, false);
+			float des = data.length;
+			des = des / size;
+			if (des <= 1) {
+				return data;
+			}
+			final float scale;
+			if (des <= 2.5F) {
+				scale = 0.95F;
+			} else if (des <= 5.0F) {
+				scale = 0.9F;
+			} else if (des <= 7.5F) {
+				scale = 0.85F;
+			} else {
+				scale = 0.8F;
+			}
+			while (true) {
+				if (null == data || data.length <= size) {
+					break;
+				}
+				Bitmap bitmapCopy = bitmap;
+				width *= scale;
+				height *= scale;
+				bitmap = Bitmap.createScaledBitmap(bitmapCopy, (int) width,
+						(int) height, true);
+				bitmapCopy.recycle();
+				data = bmpToByteArray(bitmap, false);
+			}
+		} catch (Exception e) {
+			data = null;
+			LogUtils.e(TAG,
+					"scaleBitmapIfNeededToSize Exception: " + e.getMessage());
+		}
+		return data;
 	}
 }
