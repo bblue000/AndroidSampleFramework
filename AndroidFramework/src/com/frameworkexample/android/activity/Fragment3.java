57edda45ee0d6f26423eadd0013d150fc3f8ed54
YinYong <yy15151877621@126.com>
2013-11-27 22:04:30 +0800

    [Update] 更新AndroidFramework框架

---
+++ AndroidFramework/src/com/frameworkexample/android/activity/Fragment3.java
@@ -0,0 +1,40 @@
+package com.frameworkexample.android.activity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import com.frameworkexample.android.R;
+import com.frameworkexample.android.activity.base.BaseFragment;
+
+public class Fragment3 extends BaseFragment {
+
+	@Override
+	public int getLayoutResId() {
+		return R.layout.activity_main;
+	}
+
+	@Override
+	public void initView(View view) {
+		view.setBackgroundColor(0xFF00FFFF);
+	}
+
+	@Override
+	public void initListener() {
+	}
+
+	@Override
+	public void initData(View view, Bundle savedInstanceState) {
+	}
+
+	@Override
+	public void onClick(View v) {
+	}
+
+	@Override
+	protected Handler createActivityHandler() {
+
+		return null;
+	}
+
+}
